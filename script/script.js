6c07e1243722c0e770a50e9b4ab2ddf8a6e8cb1c
Aleksey Kouzmenko <iskeyiv@gmail.com>
2019-12-17 20:25:08 +0400

    init commit

---
+++ script/script.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+    const customer = document.getElementById('customer');
+    const freelanser = document.getElementById('freelancer');
+
+    const blockCustomer = document.querySelector('#block-customer');
+    const blockFreelanser = document.querySelector('#block-freelancer');
+    const blockChoice = document.getElementById('block-choice');
+
+    const btnExit = document.getElementById('btn-exit');
+
+    const formCustomer = document.getElementById('form-customer');
+
+    const orders = [];
+
+    customer.addEventListener('click', () => {
+        blockChoice.style.display = 'none';
+        blockCustomer.style.display = 'block';
+        btnExit.style.display = 'block';
+    });
+
+    freelanser.addEventListener('click', () => {
+        blockChoice.style.display = 'none';
+        blockFreelanser.style.display = 'block';
+        btnExit.style.display = 'block';
+    });
+
+    btnExit.addEventListener('click', () => {
+        blockFreelanser.style.display = 'none';
+        blockCustomer.style.display = 'none';
+        btnExit.style.display = 'none';
+        blockChoice.style.display = 'block';
+    });
+
+    formCustomer.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const arr = Array.from(formCustomer.elements);
+        const obj = {};
+        arr.filter(el => 
+            (el.tagName === 'INPUT' && el.type !== 'radio') ||
+            (el.type === 'radio' && el.checked) ||
+            (el.tagName === 'TEXTAREA')
+        ).forEach(el => {
+            obj[el.name] = el.value;
+        });
+        arr.filter(el => el.type !== 'radio').forEach(el => el.value = '');
+        orders.push(obj);
+        console.log('orders: ', orders);
+    });
+});
